f85ec8c222f96f04f8415dd803cb4bc2b1d00f42
Geoff Matheson <geoffmatheson@gmail,com>
2024-07-22 20:39:02 +1000

    Moved the tests to their own files

--- ex2_string_exercises.py
+++ ex2_string_exercises.py
@@ -24,38 +24,3 @@ def count_the_es(s: str) -> int:
 
     return 0
 
-###
-# Tests. Don't mess with this code, just run it in the terminal by typing "pytest ex2_string_exercises.py"
-###
-
-def test_hello():
-    assert say_hello("Alice") == "Hello, Alice!"
-    assert say_hello("Bob") == "Hello, Bob!"
-    assert say_hello("Charlie") == "Hello, Charlie!"
-    assert say_hello("") == "Hello, David!"
-
-
-def test_double_the_string():
-    assert double_the_string("Hello") == "HelloHello"
-    assert double_the_string("World") == "WorldWorld"
-    assert double_the_string("Python") == "PythonPython"
-    assert double_the_string("") == ""
-    assert double_the_string("#!#AbC") == "#!#AbC#!#AbC"
-
-
-def test_reverse_string():
-    assert reverse_string("Hello") == "olleH"
-    assert reverse_string("World") == "dlroW"
-    assert reverse_string("Python") == "nohtyP"
-    assert reverse_string("") == ""
-    assert reverse_string("#!#AbC") == "CbA#!#"
-
-
-
-def test_count_the_es():
-    assert count_the_es("Hello") == 1
-    assert count_the_es("World") == 0
-    assert count_the_es("Earth") == 1
-    assert count_the_es("eeeee") == 5
-    assert count_the_es("EeEeEe") == 6
-
