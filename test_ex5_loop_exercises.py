b93a7fe1e420f453aed45849a05ac10a6819b364
Geoff Matheson <geoffmatheson@gmail,com>
2024-07-25 11:50:27 +1000

    Couple of extra tests for fun

--- test_ex5_loop_exercises.py
+++ test_ex5_loop_exercises.py
@@ -10,12 +10,15 @@ def test_list_of():
 def test_countdown():
     assert ex5.countdown(3) == "3 2 1 0"
     assert ex5.countdown(5) == "5 4 3 2 1 0"
+    assert ex5.countdown(1) == "1 0"
+    assert ex5.countdown(10) == "10 9 8 7 6 5 4 3 2 1 0"
     assert ex5.countdown(0) == "0"
 
 # Test for the sum_of_squares function
 def test_sum_of_squares():
     assert ex5.sum_of_squares(3) == 14
     assert ex5.sum_of_squares(5) == 55
+    assert ex5.sum_of_squares(1) == 1
     assert ex5.sum_of_squares(0) == 0
 
 # Test for the factorial function
