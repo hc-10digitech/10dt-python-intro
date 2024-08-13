d9e43ecf11066d22a09304fad83a25181c4903c9
Geoff Matheson <geoff.matheson@education.vic.gov.au>
2024-08-13 11:55:29 +1000

    Exercise 1

--- ex1_number_exercises.py
+++ ex1_number_exercises.py
@@ -2,42 +2,47 @@ from time import sleep
 # Python exercises. You need to write code in spots labelled "TODO:"
 def add_two_numbers(a: int, b: int) -> int:
     # TODO: Write code to add a and b. Your function needs to "return" the correct sum (instead of 0)
-
-    return 0
+    c = a + b
+    return c
 
 
 def subtract_two_numbers(a: int, b: int) -> int:
     # TODO: Write code to subtract b from a. Your function needs to "return" the correct difference
-
-    return 0
+    c = a - b
+    return c
 
 
 def multiply_two_numbers(a: int, b: int) -> int:
     # TODO: Write code to multiply a and b. Your function needs to "return" the correct product (instead of 0)
-
-    return 0
+    return a * b
 
 def which_is_greater(a: int, b: int) -> int:
     # TODO: Write code to find which number is greater. Your function needs to "return" the greater number
+    if a > b:
+        return a
+    else:
+        return b
 
-    return a
 
 def difference_between_numbers(a: int, b: int) -> int:
     # TODO: Write code to find the difference between a and b. Your function needs to "return" the correct difference
     # Note: The difference should be positive, so check which is bigger
-
-    return 0
+    if a > b:
+        return a - b
+    else:
+        return b - a
+    
 
 def raise_to_power(base, power):
     # TODO: Write code to raise the base to the power.
-
-    return 0
+    answer = base ** power
+    return answer
 
 
 if __name__ == "__main__":
     # Run each function with your own input to test them
-    a = input("Enter the value for a: ")
-    b = input("Enter the value for b: ")
+    a = int(input("Enter the value for a: "))
+    b = int(input("Enter the value for b: "))
     print("Running add_two_numbers(a, b):")
     print(f"Output: {add_two_numbers(a, b)}")
     sleep(0.3)
